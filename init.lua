4ba49d69427949ed6bd7a660ce7fea98c7226323
ldraney <ldraney@users.noreply.github.com>
2026-02-13 13:14:07 -0700

    Use TSUpdate build step for treesitter parsers

--- init.lua
+++ init.lua
@@ -868,9 +868,11 @@ require('lazy').setup({
 
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
     config = function()
+      -- Enable treesitter highlighting for supported filetypes
+      -- Parsers are installed via :TSUpdate (build step) or manually via :TSInstall
       local filetypes = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' }
-      require('nvim-treesitter').install(filetypes)
       vim.api.nvim_create_autocmd('FileType', {
         pattern = filetypes,
         callback = function() vim.treesitter.start() end,
